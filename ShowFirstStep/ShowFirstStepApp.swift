0457ba0e93930c23f3b3bf4e46a77f545272fd72
abochan123 <96906477+abochan123@users.noreply.github.com>
2022-02-13 10:21:54 +0100

    Initial Commit

---
+++ ShowFirstStep/ShowFirstStepApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShowFirstStepApp.swift
+//  ShowFirstStep
+//
+//  Created by sawako barberon on 13/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct ShowFirstStepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
